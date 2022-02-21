b82066786b8b75fdc547225c970180982e8566cc
2001arman <2001armanmaulana@gmail.com>
2022-02-21 15:52:10 +0800

    footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,34 @@
+<footer>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-5 ps-5 ms-3 mb-5 me-5">
+          <img src="{{url('img/logo-small.png')}}" alt="logo" srcset="" width=""> <br>
+          <div class="d-flex mt-4">
+            <img src="{{url('img/icon_location.png')}}" alt="location" height="24px">
+            <p class="fs-6 ms-3"> Jalan Damnhuri 2 (eks Gg Ogok), <br>
+              50 meter setelah gunung pertama sebelah kanan  <br>
+              (samping nasi goreng) </p>
+          </div>
+          <div class="d-flex">
+            <img src="{{url('img/icon_phone.png')}}" alt="phone" height="24px">
+            <p class="fs-6 ms-3"> 082338368917 </p>
+          </div>
+          <div class="d-flex ">
+            <img src="{{url('img/icon_instagram.png')}}" alt="instagram" height="24px">
+            <p class="fs-6 ms-3"> Kora Petshop </p>
+          </div>
+        </div>
+        <div class="col-3 me-3">
+          <p class="fw-bold">Lokasi</p>
+          <img src="{{url('img/image_maps.png')}}" alt="maps" srcset="">
+        </div>
+        <div class="col-3 ">
+          <p class="fw-bold">Layanan Kami</p>
+          <a href="" ><span>Pet Hotel</span></a><br>
+          <a href="" ><span> Makanan</span> </a> <br>
+          <a href="" ><span>Obat</span> </a> <br>
+          <a href="" ><span>Perlengkapan</span></a>
+        </div>
+      </div>
+    </div>
+  </footer>
