9666c37da48fe2a8f7f956967a83e4953c9c39cf
2001arman <2001armanmaulana@gmail.com>
2022-02-21 15:52:38 +0800

    login route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('kora');
+    return view('index');
 });
 
-
+Route::get('/masuk', function () {
+    return view('masuk');
+});
