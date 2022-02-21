7f8700ef3912736f580d929342f193567076e446
2001arman <2001armanmaulana@gmail.com>
2022-02-21 15:52:19 +0800

    navbar

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-light fixed-top border-bottom" style="background-color: white;">
+    <div class="container-fluid">
+      <a class="navbar-brand ms-5 me-auto" href="#"><img src="{{url('img/logo-small.png')}}" alt="logo" width="100"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse container-fluid" id="navbarNavDropdown">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">Hubungi Kami</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Layanan
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Pet Hotel</a></li>
+              <li><a class="dropdown-item" href="#">Makanan</a></li>
+              <li><a class="dropdown-item" href="#">Obat</a></li>
+              <li><a class="dropdown-item" href="#">Perlengkapan</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="navbar-brand me-5 fs-6">
+            <a class="nav-link active" aria-current="page" href="/masuk">Masuk
+              <img class="d-inline " src="{{url('img/icon_arrow.png')}}" width="20" alt="arrow" srcset="">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
