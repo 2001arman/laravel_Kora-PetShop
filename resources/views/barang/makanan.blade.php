b040db7a4322ea7a86596bc96f472d9957638fd2
2001arman <2001armanmaulana@gmail.com>
2022-03-09 18:15:53 +0800

    makanan page

--- resources/views/barang/makanan.blade.php
+++ resources/views/barang/makanan.blade.php
@@ -22,30 +22,24 @@
 
     <main>
         <!-- corousel banner -->
-        <div class="container-fluid mt-5 pt-5 px-5">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="{{ url('img/banner.png') }}" class="d-block w-100" alt="banner">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="{{ url('img/cat-big.png') }}" class="d-block w-100" alt="banner">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="{{ url('img/banner.png') }}" class="d-block w-100" alt="banner">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-        </div>  
+        
         <!-- corousel banner -->
+        <div class=" mt-5 mx-3 pt-5 px-5 d-flex flex-wrap justify-content-between ">
+            @foreach($barang as $b)
+            <div class="myCardBarang mb-4 me-3">
+                <img class="mb-1" src="{{ $b->gambar }}" alt="{{ $b->nama }}"> <br>
+                <span class="stok" >Stok Tersedia</span>
+                <p class="mb-0 mt-1 nama">{{ $b->nama }}</p>
+                <span class="harga">Rp {{ $b->harga }}</span>
+            </div>
+            @endforeach
+            <!-- <div class="myCardBarang ">
+                <img src="{{ url('img/Alpo.jpg') }}" alt="alpo"> <br>
+                <span class="stok" >Stok Tersedia</span>
+                <p class="mb-0 mt-1 nama">Alpo Beef Liver 2.5 Kg</p>
+                <span class="harga">Rp 45.000</span>
+            </div> -->
+        </div>
     </main>
     
     <!-- footer -->
@@ -58,9 +52,6 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="{{ url('js/jquery-3.6.0.js') }}"></script>
-    <script src="{{ url('js/owl.carousel.js') }}"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
