aace3c0d4e10bb460d4f66980ae4327da392155a
2001arman <2001armanmaulana@gmail.com>
2022-03-09 18:14:38 +0800

    + fix bug on gagal register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,15 +9,31 @@ use Illuminate\Support\Facades\Hash;
 class RegisterController extends Controller
 {
     protected function create(Request $data){
-        User::create([
-            'nama' => $data['name'],
-            'username' => $data['username'],
-            'email' => $data['email'],
-            'alamat' => $data['alamat'],
-            'no_hp' => $data['no_hp'],
-            'password' => Hash::make($data['password']),
-        ]);
-        session('berhasil', 'berhasil daftar');
-        return redirect('/masuk');
+        try {
+            if($data['password'] == $data['confirm-password']){
+                User::create([
+                    'nama' => $data['name'],
+                    'username' => $data['username'],
+                    'email' => $data['email'],
+                    'alamat' => $data['alamat'],
+                    'no_hp' => $data['no_hp'],
+                    'password' => Hash::make($data['password']),
+                ]);
+                session('berhasil', 'berhasil daftar');
+                return redirect('/masuk');
+            } else{
+                $data->session()->flash('error', 'Password dan Konfirmasi Password tidak sama');
+                return redirect('/daftar');
+            }
+        } catch (\Throwable $th) {
+            if($th->getCode() == 23000){
+                $data->session()->flash('error', 'username atau email sudah ada');
+                    return redirect('/daftar');
+            } else{
+                $data->session()->flash('error', $th->getMessage());
+                    return redirect('/daftar');
+            }
+        }
+        
     }
 }
