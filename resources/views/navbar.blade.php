ccb5268f58985f50b917d75e1e0a7d3709f24e4b
2001arman <2001armanmaulana@gmail.com>
2022-03-10 15:33:09 +0800

    + navbar profile and logout

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top border-bottom px-3" style="background-color: white;">
     <div class="container-fluid">
-      <a class="navbar-brand ms-5 me-auto" href="#"><img src="{{url('img/logo-small.png')}}" alt="logo" width="100"></a>
+      <a class="navbar-brand ms-5 me-auto" href="/"><img src="{{url('img/logo-small.png')}}" alt="logo" width="100"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -17,21 +17,25 @@
               Layanan
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <li><a class="dropdown-item" href="#">Pet Hotel</a></li>
-              <li><a class="dropdown-item" href="#">Makanan</a></li>
-              <li><a class="dropdown-item" href="#">Obat</a></li>
-              <li><a class="dropdown-item" href="#">Perlengkapan</a></li>
+              <li><a class="dropdown-item" href="/hotel">Pet Hotel</a></li>
+              <li><a class="dropdown-item" href="/makanan">Makanan</a></li>
+              <li><a class="dropdown-item" href="/obat">Obat</a></li>
+              <li><a class="dropdown-item" href="/perlengkapan">Perlengkapan</a></li>
             </ul>
           </li>
         </ul>
       </div>
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         @if(session()->has('user'))
-          <ul class="navbar-nav">
-            <li class="navbar-brand me-5 fs-6">
-              <a class="nav-link active" aria-current="page" href="/profile">{{ session()->get('user')['nama'] }}
-                <img class="d-inline " src="{{url('img/icon_arrow.png')}}" width="20" alt="arrow" srcset="">
+          <ul class="navbar-nav me-5 fs-6">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle fw-semiBold" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {{ session()->get('user')['nama'] }}
               </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <li><a class="dropdown-item" href="/profile">Profile</a></li>
+                <li><a class="dropdown-item" href="/logout">Logout</a></li>
+              </ul>
             </li>
           </ul>
         @else
