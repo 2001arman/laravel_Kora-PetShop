a3a3520b03989c136e41ffea92d08af344b89211
smtekien <sulthonrahmatullah@gmail.com>
2022-03-11 10:18:28 +0800

    add carousel

---
+++ public/js/script.js
@@ -0,0 +1,13 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 20,
+  stagePadding: 180,
+  nav: false,
+  mouseDrag: true,
+  touchDrag: true,
+  responsive: {
+    0:{
+      items: 1
+    }
+  }
+})
