ca1f767c4c0c97e5ba63b2291a7a6c697887abef
2001arman <2001armanmaulana@gmail.com>
2022-03-13 19:22:31 +0800

    + onclick to details page

--- resources/views/barang/obat.blade.php
+++ resources/views/barang/obat.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{url('css/style.css')}}">
     <link rel="stylesheet" href="{{url('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{url('css/owl.theme.default.css')}}">
     <link rel="shortcut icon" href="{{url('img/logo-small.png')}}" type="image/x-icon">
 
     <title>Kora Pet Hotel</title>
@@ -21,25 +22,31 @@
     </header>
 
     <main>
-        <!-- corousel banner -->
-        
-        <!-- corousel banner -->
-        <div class=" mt-5 mx-3 pt-5 px-5 d-flex flex-wrap justify-content-between ">
-            @foreach($barang as $b)
-            <div class="myCardBarang mb-4 me-3">
-                <img class="mb-1" src="{{ $b->gambar }}" alt="{{ $b->nama }}"> <br>
-                <span class="stok" >Stok Tersedia</span>
-                <p class="mb-0 mt-1 nama">{{ $b->nama }}</p>
-                <span class="harga">Rp {{ $b->harga }}</span>
-            </div>
-            @endforeach
-            <!-- <div class="myCardBarang ">
-                <img src="{{ url('img/Alpo.jpg') }}" alt="alpo"> <br>
-                <span class="stok" >Stok Tersedia</span>
-                <p class="mb-0 mt-1 nama">Alpo Beef Liver 2.5 Kg</p>
-                <span class="harga">Rp 45.000</span>
-            </div> -->
+      <!-- corousel banner -->
+      <div class="mt-5 mx-3 pt-5 px-5 d-flex flex-wrap justify-content-between">
+        <div class="owl-carousel owl-theme">
+          <div class="item">
+            <img src="{{ url('img/carousel-image.jpg') }}" class="carousel-img" alt="">
+          </div>
+          <div class="item">
+            <img src="{{ url('img/carousel-image.jpg') }}" class="carousel-img" alt="">
+          </div>
+          <div class="item">
+            <img src="{{ url('img/carousel-image.jpg') }}" class="carousel-img" alt="">
+          </div>
         </div>
+      </div>
+      <!-- corousel banner -->
+      <div class=" mt-5 mx-3 pt-5 px-5 d-flex flex-wrap justify-content-between ">
+          @foreach($barang as $b)
+          <div class="myCardBarang mb-4 me-3" onclick="location.href='/detail/{{ $b->id }}'">
+              <img class="mb-1" src="{{ $b->gambar }}" alt="{{ $b->nama }}"> <br>
+              <span class="stok" >Stok Tersedia</span>
+              <p class="mb-0 mt-1 nama">{{ $b->nama }}</p>
+              <span class="harga">Rp {{ number_format($b->harga , 0, ',', '.') }}</span>
+          </div>
+          @endforeach
+      </div>
     </main>
     
     <!-- footer -->
@@ -52,6 +59,8 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="{{ url('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ url('js/script.js') }}"></script>
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
