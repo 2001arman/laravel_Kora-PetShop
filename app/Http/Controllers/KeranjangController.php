743a43ea3ba06705c69bdde0b7ac66d1a9f74602
2001arman <2001armanmaulana@gmail.com>
2022-03-15 14:48:43 +0800

    store data and session

--- app/Http/Controllers/KeranjangController.php
+++ app/Http/Controllers/KeranjangController.php
@@ -12,17 +12,34 @@ class KeranjangController extends Controller
         // masukkan data ke db keranjang
         try {
             $isExist = DB::table('keranjang')->where([
-                ['id-barang', '=', $barang],
-                ['id-user', '=', $user],
+                ['id_barang', '=', $barang],
+                ['id_user', '=', $user],
                 ])->exists();
             if($isExist != true){
                 try {
                     DB::table('keranjang')->insert([
-                        'id-barang' => $barang,
-                        'id-user' => $user,
+                        'id_barang' => $barang,
+                        'id_user' => $user,
                         'jumlah' => 1,
                     ]);
-                    session()->flash('error', 'data kosong berhasil masukin data');
+                    $dataBarang = DB::table('barang')->where('id', $barang)->get();
+                    if(session()->has('jumlah')){
+                        $jumlah = session()->get('jumlah') + 1;
+                        $total = session()->get('total');
+                        foreach($dataBarang as $b){
+                            $total = $total + $b->harga;
+                        }
+                        session()->put('jumlah', $jumlah);
+                        session()->put('total', $total);
+                    } else{
+                        $jumlah = 1;
+                        foreach($dataBarang as $b){
+                            $total = $b->harga;
+                        }
+                        session()->put('jumlah', $jumlah);
+                        session()->put('total', $total);
+                    }
+                    session()->flash('berhasil', 'Berhasil memasukkan barang ke keranjang');
                     return redirect("/detail/$barang");
                 } catch (\Throwable $th) {
                     session()->flash('error', $th->getMessage());
@@ -30,7 +47,18 @@ class KeranjangController extends Controller
                 }
                 
             } else{
-                session()->flash('error', $isExist);
+                $tambahJumlah = DB::table('keranjang')->where([
+                    ['id_barang', '=', $barang],
+                    ['id_user', '=', $user],
+                    ])->get();
+                foreach($tambahJumlah as $tambah){
+                    $jumlah = $tambah->jumlah;
+                    DB::table('keranjang')->where([
+                        ['id_barang', '=', $barang],
+                        ['id_user', '=', $user],
+                        ])->update(['jumlah' => $jumlah+1 ]);
+                }
+                session()->flash('berhasil', 'Berhasil menambahkan jumlah barang');
                 return redirect("/detail/$barang");
             }
         } catch (\Throwable $th) {
