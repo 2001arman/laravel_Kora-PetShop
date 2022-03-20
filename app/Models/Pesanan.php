638247170da046f14f8a74a2609f794e208a3f5b
arman <2001armanmaulana@gmail.com>
2022-03-21 07:35:01 +0800

    pesanan model

---
+++ app/Models/Pesanan.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pesanan extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id_barang',
+        'id_user',
+        'jumlah',
+    ];
+}
