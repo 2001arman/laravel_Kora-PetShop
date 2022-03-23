b4f3af110a79ccb069fb855d272f22e7f8960131
2001arman <2001armanmaulana@gmail.com>
2022-03-23 12:40:20 +0800

    + function checkbox and button plus minus

--- public/js/app.js
+++ public/js/app.js
@@ -28,3 +28,52 @@ function validateCaptcha() {
         document.getElementById("form_regis").submit(); /*disini kuganti jadi apabila berhasil dia bakal subtmit form */
     }
   }
+
+
+function toggle(source) {
+  checkboxes = document.getElementsByName('checkboxBarang');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
+
+function minus(id, harga){
+  textJumlahBarang1 = document.getElementById("jumlahBarangKeranjang1");
+  textJumlahBarang2 = document.getElementById("jumlahBarangKeranjang2");
+  inputJumlah = document.getElementById(id.id);
+  value = parseInt(inputJumlah.value);
+  console.log(harga);
+  if(value > 1){
+    inputJumlah.value = value - 1;
+    spanTotal1 = document.getElementById("totalHarga1");
+    spanTotal2 = document.getElementById("totalHarga2");
+    stringTotal = spanTotal1.innerText;
+    newTotal = stringTotal.split('.').join("");
+    hasil = parseInt(newTotal) - harga ;
+    jumlahBarang = parseInt(textJumlahBarang2.innerText);
+    jumlahBarang = jumlahBarang - 1;
+    spanTotal1.innerHTML = hasil.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    spanTotal2.innerHTML = hasil.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    textJumlahBarang1.innerHTML = "Total Harga (" + jumlahBarang + " Barang)";
+    textJumlahBarang2.innerHTML = jumlahBarang;
+  }
+}
+
+function plus(id, harga){
+  textJumlahBarang1 = document.getElementById("jumlahBarangKeranjang1");
+  textJumlahBarang2 = document.getElementById("jumlahBarangKeranjang2");
+  inputJumlah = document.getElementById(id.id);
+  value = parseInt(inputJumlah.value);
+  inputJumlah.value = value + 1;
+  spanTotal1 = document.getElementById("totalHarga1");
+  spanTotal2 = document.getElementById("totalHarga2");
+  stringTotal = spanTotal1.innerText;
+  newTotal = stringTotal.split('.').join("");
+  hasil = parseInt(newTotal) + harga ;
+  jumlahBarang = parseInt(textJumlahBarang2.innerText);
+  jumlahBarang = jumlahBarang + 1;
+  spanTotal1.innerHTML = hasil.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  spanTotal2.innerHTML = hasil.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  textJumlahBarang1.innerHTML = "Total Harga (" + jumlahBarang + " Barang)";
+  textJumlahBarang2.innerHTML = jumlahBarang;
+}
